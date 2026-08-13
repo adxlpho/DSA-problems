a94a9d4d40611e3a29e6520296e3aa22038b9a6f
Adolpho Ramirez <ptech.adolphoramirez@gmail.com>
2026-08-12 23:23:32 -0500

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        // Check if the array is sorted or null
+
+        // Sort it
+        Arrays.sort(nums);
+
+        // Check if the first and last elements are the same
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
